131de252ced21516c41c44e62fd5bb029a74c27f
Junaid Oloruntobi <easytobeh@gmail.com>
2021-02-20 18:24:53 +0100

    Added URL to fetch User data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Welcome Junaid</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        
+      ]
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+          <div>
+            {
+              this.state.monsters.map(monster => <div key={monster.id} >{monster['name']}</div>
+              )
+            }
+            </div>
+       </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
